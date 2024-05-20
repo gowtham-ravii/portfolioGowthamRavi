08956796b1f3f9d6cff43372511223f7fec7ac9f
GOWTHAM R <100552250+gowtham-ravii@users.noreply.github.com>
2024-05-20 14:36:07 +0530

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -6,19 +6,19 @@ export const featuresData = [
     id: 1,
     icon: <FaReact />,
     title: "Full Stack Development",
-    des: "Experienced in MERN and React Native, I've completed five full-stack projects during my education, showcasing proficiency in both front-end and back-end development.",
+    des: "Experienced in MERN and React-native basics, I've completed some full-stack projects during my education, showcasing proficiency in both front-end and back-end development.",
   },
   {
     id: 2,
     icon: <FaJava />,
     title: "Java",
-    des: "Passionate about Java and Object-Oriented Programming (OOP), I have practical experience working on Java-based projects using Android Studio.",
+    des: "Passionate about Java and Object-Oriented Programming (OOP), I have practical experience working on Java by developing consolebased applications.",
   },
   {
     id: 3,
 
     title: "Data Structures",
-    des: "I consistently practice Data Structures and Algorithms (DSA) and have solved over 400+ problems on platforms like LeetCode, enhancing my problem-solving skills.",
+    des: "I consistently practice Data Structures and Algorithms (DSA) and have solved over 400+ problems on platforms like LeetCode,CodeStudio,GeeksForGeeks enhancing my problem-solving skills.",
   },
   
   
